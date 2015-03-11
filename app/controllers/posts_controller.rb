a41876ab4c4c7c857477046561d09c38cdd7898e
tencet <tencet@yandex.ru>
2015-03-11 16:36:44 +0800

    Changed sorting posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.order(:created_at).reverse_order
   end
 
   # GET /posts/1
--- app/controllers/tencet_controller.rb
+++ app/controllers/tencet_controller.rb
@@ -1,5 +1,9 @@
 class TencetController < ApplicationController
+
+  http_basic_authenticate_with name: "tencet", password: "qk35lm"
+
   def index
-    @posts = Post.all
+    @posts = Post.order(:created_at).reverse_order
   end
+
 end
