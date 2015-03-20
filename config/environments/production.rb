bf19a24d2a4ea93dd5505f42fbf77f40744c20ff
tencet <tencet@yandex.ru>
2015-03-20 16:32:03 +0800

    Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,15 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'bestmusic.herokuapp.com' }
-
   config.action_mailer.smtp_settings = {
-  :address   => "smtp.mandrillapp.com",
-  :port      => 25,
-  :user_name => "tencet@yandex.ru",
-  :password  => "va3DEr2tBDl6ZqL1FDiJLg"
-}
+    :address   => "smtp.mandrillapp.com",
+    :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
+    :enable_starttls_auto => true, # detects and uses STARTTLS
+    :user_name => "tencet@yandex.ru",
+    :password  => "va3DEr2tBDl6ZqL1FDiJLg", # SMTP password is any valid API key
+    :authentication => 'login', # Mandrill supports 'plain' or 'login'
+    :domain => 'bestmusic.herokuapp.com', # your domain to identify your server when connecting
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
